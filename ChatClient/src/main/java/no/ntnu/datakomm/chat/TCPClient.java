4c4f166251e89482e76d18b585d71752d0529bdf
Aron <aronselven@gmail.com>
2021-11-01 21:49:56 +0100

    Completed step 6

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -147,7 +147,16 @@ public class TCPClient {
         // TODO Step 6: Implement this method
         // Hint: Reuse sendCommand() method
         // Hint: update lastError if you want to store the reason for the error.
-        return false;
+        boolean success = false;
+        if (recipient.trim().isBlank() || message.trim().isBlank()){
+            this.lastError = "Recipient or message is blank.";
+        } else {
+            success = sendCommand("privmsg " + recipient + " " + message);
+        }
+        if (!success){
+            this.lastError = "Private message not sent.";
+        }
+        return success;
     }
 
 
